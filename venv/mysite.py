a845f7f9e723b1e9b6e9c129dfeb4125a9a59019
HSGod <1138112674@qq.com>
2020-07-07 13:45:45 +0800

    3html

---
+++ venv/mysite.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from werkzeug.routing import BaseConverter
+
+
+
+app = Flask(__name__)
+#读取配置文件
+app.config.from_pyfile('config.ini')
+
+#正则表达式
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *args):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = args[0]
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method ==  'POST':
+        username = request.form.get('UserName')
+        password = request.form.get('Password')
+        #return render_template('register.html')
+    return render_template('login.html')
+
+app.url_map.converters['re'] = RegexConverter
+
+@app.route('/orders/<re("[0-9]{3}"):order_id>', methods=['GET','POST'])
+def get_order_id(order_id):
+    return "order id is %s" % order_id
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    return render_template('prediction.html')
+
+#返回json
+@app.route('/demo3')
+def demo3():
+    return jsonify('./static/data/min.json')
+
+@app.route('/demo4')
+def demo4():
+    return redirect('http://www.baidu.com')
+
+@app.route('/demo5')
+def demo5():
+    return redirect(url_for('index'))
+#启动服务器
+if __name__ == '__main__':
+    app.run()
